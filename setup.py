00df45833a35f5c39ac6ee973fc8d99df0acbe18
satyajeetrai007 <satyajeetrai0@gmail.com>
2025-07-23 21:32:28 +0530

    initial setup commit no code added just the template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Project to practically implement whatever i have learned from lectures till model registry',
+    author='Satyajeet',
+    license='MIT',
+)
